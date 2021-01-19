cc29ea82fd33b68b8c9b20fb6390069f9e96f96d
Norbert Boros <boros.norbee@gmail.com>
2021-01-19 18:11:48 +0100

    Add SpringDoc to the project

--- src/main/kotlin/com/norbi930523/trello/common/api/StatusRestController.kt
+++ src/main/kotlin/com/norbi930523/trello/common/api/StatusRestController.kt
@@ -1,19 +1,22 @@
 package com.norbi930523.trello.common.api
 
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.responses.ApiResponse
+import io.swagger.v3.oas.annotations.responses.ApiResponses
+import io.swagger.v3.oas.annotations.tags.Tag
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
-/**
- * API endpoints to check the status of the application, e.g. readiness, liveness.
- */
 @RestController
 @RequestMapping("/status")
+@Tag(name = "status", description = "API endpoints to check the status of the application, e.g. readiness, liveness.")
 class StatusRestController {
 
-    /**
-     * Check if the application is initialized and ready to accept connections.
-     */
+    @Operation(description = "Check if the application is initialized and ready to accept connections.")
+    @ApiResponses(value = [
+        ApiResponse(responseCode = "200", description = "The application is ready.")
+    ])
     @GetMapping("/ready")
     fun ready() = "OK"
 
